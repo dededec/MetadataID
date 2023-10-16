2ea59020900edeb3f30b4eb4da977d120536424c
dededec <daviderenacasado@gmail.com>
2023-10-16 17:55:09 +0200

    First commit

---
+++ src/main/java/com/dededec/metadataid/MetadataidApplication.java
@@ -0,0 +1,13 @@
+package com.dededec.metadataid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetadataidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetadataidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dededec/metadataid/MetadataidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dededec.metadataid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetadataidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
