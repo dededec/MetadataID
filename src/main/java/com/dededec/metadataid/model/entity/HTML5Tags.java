9df690c25e194d8d055fadbcab568cdd3e308112
dededec <daviderenacasado@gmail.com>
2023-10-18 21:01:21 +0200

    Update HTML5 tags

--- src/main/java/com/dededec/metadataid/model/entity/HTML5Tags.java
+++ src/main/java/com/dededec/metadataid/model/entity/HTML5Tags.java
@@ -2,8 +2,29 @@ package com.dededec.metadataid.model.entity;
 
 public enum HTML5Tags {
     ARTICLE("article"),
+    ASIDE("aside"),
+    AUDIO("audio"),
+    CANVAS("canvas"),
+    COMMAND("command"),
+    DATALIST("datalist"),
+    DETAILS("details"),
+    EMBED("embed"),
+    FIGURE("figure"),
+    FOOTER("footer"),
     HEADER("header"),
-    FOOTER("footer");
+    HGROUP("hgroup"),
+    KEYGEN("keygen"),
+    MARK("mark"),
+    METER("meter"),
+    NAV("nav"),
+    OUTPUT("output"),
+    PROGRESS("progress"),
+    RUBY("ruby"),
+    SECTION("section"),
+    TIME("time"),
+    VIDEO("video"),
+    WBR("wbr");
 
-    private HTML5Tags(String tag) {}
+    private HTML5Tags(String tag) {
+    }
 }
